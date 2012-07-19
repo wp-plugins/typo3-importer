d451c39e07fff18d03d3170372da764c175b8543
comprock <comprock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-19 12:38:34 +0000

    Copy "trunk/" to "tags/2.0.6"
    git-svn-id: https://plugins.svn.wordpress.org/typo3-importer/tags/2.0.6@574574 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- typo3-importer.php
+++ typo3-importer.php
@@ -3,7 +3,7 @@
 Plugin Name: TYPO3 Importer
 Plugin URI: http://wordpress.org/extend/plugins/typo3-importer/
 Description: Import tt_news and tx_comments from TYPO3 into WordPress.
-Version: 2.0.4
+Version: 2.0.6
 Author: Michael Cannon
 Author URI: http://typo3vagabond.com/about-typo3-vagabond/hire-michael/
 License: GPL2
